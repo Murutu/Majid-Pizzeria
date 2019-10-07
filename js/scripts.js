316ae66aa1e12d2ac4f5c784b802380a602c92af
Murutu <murutupeter@email.com>
2019-10-07 12:11:29 +0300

    add a display function for the order summary

--- js/scripts.js
+++ js/scripts.js
@@ -1,49 +1,101 @@
-$(document).ready(function(){
-    $("#d1-show").click(function(){
-        $("#k-hidden").toggle();
-    });
+// $(document).ready(function(){
+//     $("#d1-show").click(function(){
+//         $("#k-hidden").toggle();
+//     });
 
 
-    $("#d2-show").click(function(){
-        $("#l-hidden").toggle();
-    });
+//     $("#d2-show").click(function(){
+//         $("#l-hidden").toggle();
+//     });
 
 
-    $("#d3-show").click(function(){
-        $("#m-hidden").toggle();
-    });
+//     $("#d3-show").click(function(){
+//         $("#m-hidden").toggle();
+//     });
 
 
-    $("#d4-show").click(function(){
-        $("#n-hidden").toggle();
-    });
-    
-});
+//     $("#d4-show").click(function(){
+//         $("#n-hidden").toggle();
+//     });
+
+// });
 
     //business logic
-    function Order(name, pizza,  crust,  delivery) {
-        this.customerName = name;
-        this.selectedPizza = pizza;
-        this.selectedCrust = crust;
-        this.deliveryOption = delivery;
-      }
-      function Address(location,){
-        this.locationName = location;
-        {
-            function Price(pizzaPrice){
-                this.priceForSelectedPizza = pizzaPrice;
-              }
-              Price.prototype.subtotal = function(){
-                return this.priceForSelectedPizza ;
-              }
-    //user interface
-    $(document).ready(function(){
-        $('#sizeoption').submit(function(event){
-          event.preventDefault();
-          var inputtedName = $('input#pass').val();
-          var selectedPizza = parseInt($('select#sizeoption').val());
-          var pizzas = ['Talioni:SMALL','Talioni:MEDIUM','Talioni:LARGE','Barbacao:SMALL','Barbacao:MEDIUM','Barbacao:LARGE','Chicken Silica:SMALL','Chicken Silica:MEDIUM','Chicken Silica:LARGE','Cupon:SMALL','Cupon:MEDIUM','Cupon:SMALL'];
-          var deliveryOptions = ['Collect', 'Deliver'];
-          var pizzaPrices = [400, 600, 1000, 600, 800, 1200, 650, 850, 1250, 500, 700, 1100, 550, 750, 1150, 700, 900, 1300];
-        
+    // function Order(name, pizza,  crust,  delivery) {
+    //     this.customerName = name;
+    //     this.selectedPizza = pizza;
+    //     this.selectedCrust = crust;
+    //     this.deliveryOption = delivery;
+    //   }
+    //   function Address(location,){
+    //     this.locationName = location;
+    //     {
+    //         function Price(pizzaPrice){
+    //             this.priceForSelectedPizza = pizzaPrice;
+    //           }
+    //           Price.prototype.subtotal = function(){
+    //             return this.priceForSelectedPizza ;
+    //           }
+
+    //         }
+    //     }
+ //user interface
+ $("document").ready( function(){
+
+
+function calculateTotal(size,topping,crust,quantity){
+
+    var result= size * quantity;
+    
+    var total = result + topping +crust;
+
+    return total;
+
+
+}
+
+         $("form").submit(function(event){
+                 event.preventDefault();
+
+                // var trial = $("input[name='top']:checked").val();
+                // console.log(trial);
+
+                 var sizePrice=$("input[name='size']:checked").val();
+                 var sizePart=sizePrice.split(", ");
+                 var size=parseInt(sizePart[1]);
+
+                 var toppingPrice=$("input[name='top']:checked").val();
+                 console.log(toppingPrice);
+                 var toppingPart=toppingPrice.split(", ");
+                 var topping=parseInt(toppingPart[1]);
+
+                 var crustPrice=$("input[name='crusty']:checked").val();
+                 var crustPart=crustPrice.split(", ");
+                 var crust=parseInt(crustPart[1]);
+
+                var quantity=parseInt($(".quantity").val());
+
+                
+            
+              var amount=calculateTotal(size,topping,crust,quantity);
+              alert("Your order will be delivered to your location");
+
+                 $(".display-size").text(sizePrice);
+                 $(".display-quantity").text(quantity);
+                 $(".display-toppings").text(toppingPrice);
+                 $(".display-crusts").text(crustPrice);
+                 $(".display-total").text(amount);
+
+            //      var size = $("#sizes").find(":selected").val();
+            //      var crust = $("#crusts").find(":selected").val();
+            //      var toppings = [];
+            //      $('#toppings :checked').each(function() {
+            //        toppings.push($(this).val());
+            //      });
+            //      alert(toppings)
+            //  }
+         });
+     }
+ );
+
 
